61a1119e11c187d9e84708042520bdbd647cfeca
Natasha Buckham <natasha.buckham@softwire.com>
2024-11-25 14:12:15 +0000

    Add more CRUD functions

--- main.py
+++ main.py
@@ -44,9 +44,35 @@ def add_item(item_name: str, quantity: int) -> dict[str, ItemPayload]:
     return {"item": grocery_list[item_id]}
 
 # Route to list a specific item by ID
+@app.get("/items/{item_id}")
+def list_item(item_id: int) -> dict[str, ItemPayload]:
+    if item_id not in grocery_list:
+        raise HTTPException(status_code=404, detail="Item not found.")
+    return {"item": grocery_list[item_id]}
 
 # Route to list all items
+@app.get("/items")
+def list_items() -> dict[str, dict[int, ItemPayload]]:
+    return {"items": grocery_list}
 
 # Route to delete a specific item by ID
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int) -> dict[str, str]:
+    if item_id not in grocery_list:
+        raise HTTPException(status_code=404, detail="Item not found.")
+    del grocery_list[item_id]
+    return {"result": "Item deleted."}
 
 # Route to remove some quantity of a specific item by ID
+@app.delete("items/{item_id}/{quantity}")
+def remove_quantity(item_id: int, quantity: int) -> dict[str, str]:
+    if item_id not in grocery_list:
+        raise HTTPException(status_code=404, detail="Item not found.")
+    # if quantity to be removed is higher or equal to item's quantity, delete the item
+    if grocery_list[item_id].quantity <= quantity:
+        del grocery_list[item_id]
+        return {"result": "Item deleted."}
+    else:
+        grocery_list[item_id].quantity -= quantity
+    # 'f' denotes string interpolation in Python
+    return {"result": f"{quantity} items removed."}
